4926177c6f29b677f8179185d9290110e269927e
Daniel González Cerviño <daniel.gonzalez@freelancemadrid.es>
2013-05-05 21:00:18 +0200

    add more test to sanitizer handler

---
+++ tests/Desarrolla2/RSSClient/Handler/Sanitizer/Test/SanitizerHandlerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of the RSSClient proyect.
+ * 
+ * Copyright (c)
+ * Daniel González <daniel.gonzalez@freelancemadrid.es> 
+ * 
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ */
+
+namespace Desarrolla2\RSSClient\Handler\Sanitizer\Test;
+
+use \Desarrolla2\RSSClient\Handler\Sanitizer\SanitizerHandler;
+
+/**
+ * 
+ * Description of SanitizerHandlerTest
+ *
+ * @author : Daniel González <daniel.gonzalez@freelancemadrid.es> 
+ * @file : SanitizerHandlerTest.php , UTF-8
+ * @date : May 5, 2013 , 8:55:36 PM
+ */
+class SanitizerHandlerTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \Desarrolla2\RSSClient\Handler\Error\ErrorHandler
+     */
+    protected $handler = null;
+
+    /**
+     * Setup
+     */
+    public function setUp()
+    {
+        $this->handler = new SanitizerHandler();
+    }
+
+    /**
+     * Provider
+     * 
+     * @return array
+     */
+    public function dataProvider()
+    {
+        return array(
+            array(
+                '', ''
+            )
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider dataProvider
+     */
+    public function testDoClean($expected, $data)
+    {
+        $this->assertEquals($expected, $this->handler->doClean($data));
+    }
+
+}
